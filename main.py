127988a61b23fb5927fbca0679b6eded32e8e3e6
Luis Lessa <prof.lpjunior@gmail.com>
2024-06-07 22:05:55 -0300

    WIP: aula 02

---
+++ main.py
@@ -0,0 +1,22 @@
+﻿# lista e tuplas
+lista = [1, 2, 2, 3, 4, 5]  # lista
+tupla = tuple(lista) # converte lista em tupla
+
+print(lista)
+print(tupla)
+
+# dicionário
+dicionario = {"nome": "João", "idade": 20}
+print(dicionario)
+print(dicionario["nome"])
+
+# conjuntos
+numeros = {1, 2, 2, 3, 4, 5}
+print(numeros)
+
+for numero in numeros:
+    if numero == 2:
+        print(numero)
+
+lista_convertida_em_conjunto = set(lista)
+print(lista_convertida_em_conjunto)
